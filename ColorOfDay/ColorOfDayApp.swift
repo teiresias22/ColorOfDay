609b4613f0c33d3df3524f94bc0146af75964046
Teiresias22 <teiresias1987@gmail.com>
2022-04-17 23:53:57 +0900

    Initial Commit

---
+++ ColorOfDay/ColorOfDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorOfDayApp.swift
+//  ColorOfDay
+//
+//  Created by Joonhwan Jeon on 2022/04/17.
+//
+
+import SwiftUI
+
+@main
+struct ColorOfDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
